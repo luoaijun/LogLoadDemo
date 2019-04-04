bfa4a235d96b6201bdac6598a4472f571b2c0f82
luoaijun <3191287315@qq.com>
2019-04-04 10:11:10 +0800

    新增hdfs操作Utils

--- com/cdes/service/DataWriter.py
+++ com/cdes/service/DataWriter.py
@@ -1,47 +1,53 @@
-
 import time
 import os.path
+from hdfs import InsecureClient
 
 from com.cdes.utils.HdfsUtils import HDFS
 
 
 class DateUtil:
-    def getFileByDate(self,message):
-        #获得当前系统时间的字符串
-        localtime=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+    def getFileByDate(self, message):
+        client = InsecureClient("http://10.0.75.1:50070/", user='hdfs')
+
+        # 获得当前系统时间的字符串
+        localtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
         # print('localtime='+localtime)
-        #系统当前时间年份
-        year=time.strftime('%Y',time.localtime(time.time()))
-        #月份
-        month=time.strftime('%m',time.localtime(time.time()))
-        #日期
-        day=time.strftime('%d',time.localtime(time.time()))
-        #具体时间 小时分钟毫秒
-        mdhms=time.strftime('%Y%m%d',time.localtime(time.time()))
+        # 系统当前时间年份
+        year = time.strftime('%Y', time.localtime(time.time()))
+        # 月份
+        month = time.strftime('%m', time.localtime(time.time()))
+        # 日期
+        day = time.strftime('%d', time.localtime(time.time()))
+        # 具体时间 小时分钟毫秒
+        mdhms = time.strftime('%Y%m%d', time.localtime(time.time()))
 
-        fileYear='/data/node/'+year
-        fileMonth=fileYear+'/'+month
-        fileDay=fileMonth+'/'+day
+        fileYear = '/data/node/' + year
+        fileMonth = fileYear + '/' + month
+        fileDay = fileMonth + '/' + day
+        hdfs = HDFS()
+        if not hdfs.exists(client, fileYear):
 
-        if not os.path.exists(fileYear):
-            hdfs = HDFS()
-            hdfs.mkdirs(fileYear)
-            hdfs.mkdirs(fileMonth)
-            hdfs.mkdirs(fileDay)
+            hdfs.mkdirs(client, fileYear)
+            hdfs.mkdirs(client, fileMonth)
+            hdfs.mkdirs(client, fileDay)
         else:
-            if not os.path.exists(fileMonth):
-                os.mkdir(fileMonth)
-                os.mkdir(fileDay)
+            if not hdfs.exists(client, fileMonth):
+                os.mkdir(client, fileMonth)
+                os.mkdir(client, fileDay)
             else:
-                if not os.path.exists(fileDay):
-                    os.mkdir(fileDay)
+                if not hdfs.exists(client, fileDay):
+                    os.mkdir(client, fileDay)
 
-        #创建一个文件，以‘timeFile_’+具体时间为文件名称
-        fileDir=fileDay+'/access_'+mdhms+'.txt'
-        out=open(fileDir,'a+',encoding='utf-8')
-        #在该文件中写入当前系统时间字符串
-        out.write(message)
+        # 创建一个文件，以‘timeFile_’+具体时间为文件名称
+        fileDir = fileDay + '/access_' + mdhms + '.log'
+
+        out = open(fileDir, 'a+', encoding='utf-8')
+        # 在该文件中写入当前系统时间字符串
+        client.append_to_hdfs(fileDir,client,message)
+        # out.write(message)
         out.close()
+
+
 if __name__ == '__main__':
     utils = DateUtil()
-    utils.getFileByDate("你好",)
+    utils.getFileByDate("你好", )
--- com/cdes/utils/HdfsUtils.py
+++ com/cdes/utils/HdfsUtils.py
@@ -8,7 +8,6 @@ from hdfs.client import Client, InsecureClient
 
 class HDFS:
     # 读取hdfs文件内容,将每行存入数组返回
-    client = InsecureClient("http://10.0.75.1:50070/", user='hdfs')
 
     def read_hdfs_file(client, filename):
         # with client.read('samples.csv', encoding='utf-8', delimiter='\n') as reader:
@@ -44,8 +43,9 @@ class HDFS:
             client.rm(remotepath, recursive=True)
 
     # 删除hdfs文件
-    def delete_hdfs_file(client, hdfs_path):
-        client.delete(hdfs_path)
+    def delete_hdfs_file(self,client, hdfs_path):
+        if self.exists(hdfs_path):
+            client.delete(hdfs_path)
 
     # 上传文件到hdfs
     def put_to_hdfs(client, local_path, hdfs_path):
@@ -69,6 +69,7 @@ class HDFS:
 
     # 返回目录下的文件
     def list(client, hdfs_path):
+
         return client.list(hdfs_path, status=False)
 
 
